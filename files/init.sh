0cea7c8efaa5d389f6b4de69a4f58cca9d9912da
Fabrice Brito <fabrice.brito@terradue.com>
2026-01-13 13:55:22 +0100

    fix init container, adds taskfile, k8s job manifest

--- files/init.sh
+++ files/init.sh
@@ -2,14 +2,31 @@
 
 set -x 
 
+mkdir -p /workspace/getting-started
+cd /workspace/getting-started
+
+curl -L https://github.com/EO-DataHub/dev-platform-eoap/archive/refs/heads/main.tar.gz \
+| tar -xz \
+    --strip-components=2 \
+    dev-platform-eoap-main/getting-started
+
+cp app-water-bodies-cloud-native.1.1.0.cwl /calrissian
+
+cat << EOF > /calrissian/app-water-bodies-cloud-native-params.yaml
+stac_items:
+- "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20210708_0_L2A"
+- "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2B_10TFK_20210713_0_L2A"
+- "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20210718_0_L2A"
+- "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20220524_0_L2A"
+- "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20220514_0_L2A"
+- "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20220504_0_L2A"
+aoi: -121.399,39.834,-120.74,40.472
+epsg: "EPSG:4326"
+EOF
+
 cd /workspace
 
-git clone --no-checkout https://github.com/EO-DataHub/dev-platform-eoap.git eodh-uk
-cd eodh-uk
-git sparse-checkout init --cone
-git sparse-checkout set getting-started 
-git checkout
-
+ln -s /calrissian 
 code-server --install-extension ms-python.python --install-extension redhat.vscode-yaml --install-extension sbg-rabix.benten-cwl --install-extension ms-toolsai.jupyter
 
 mkdir -p /workspace/.cwlwrapper
@@ -20,7 +37,7 @@ maincwl="/etc/cwl-wrapper-conf/main.yaml"
 rulez="/etc/cwl-wrapper-conf/rules.yaml"
 EOF
 
-cat <<EOF > /workspace/pod-node-selector.yaml
+cat <<EOF > /calrissian/pod-node-selector.yaml
 eks.amazonaws.com/nodegroup: eodhp-nFi8LsXy-eu-west-2b-Services
 EOF
 
